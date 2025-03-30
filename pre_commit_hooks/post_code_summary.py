f81890fe1115035fd16e7f551c45b2a11a09329a
meliksahuomer <73441624+melikuomer@users.noreply.github.com>
2025-03-31 01:01:11 +0300

    added file name support

--- pre_commit_hooks/post_code_summary.py
+++ pre_commit_hooks/post_code_summary.py
@@ -1,14 +1,30 @@
 
 import os
-def main():
+from typing import Sequence
+import argparse
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'filenames', nargs='*',
+        help='Filenames pre-commit believes are changed.',
+    )
+    args = parser.parse_args(argv)
 
     import subprocess
-    diff_input = subprocess.run(['git', 'diff', '-U5'], capture_output=True, text=True).stdout
+    diff_input = subprocess.run(('git', 'diff', '-U5'),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            encoding='utf-8',
+            check=True,
+            input='\0'.join(args.filenames),
+        ).stdout
 
     from pre_commit_hooks.ui import display_artifact
     from pre_commit_hooks.client import App
     client = App(api_key=os.getenv('GOOGLE_API_KEY') or "")
     artifact = client.run(diff_input)
+    print(artifact.model_dump_json())
     display_artifact(artifact)
     return 0
 
