8db89cdb8b55b0798c8b4cd65541c0f70848cd1b
meliksahuomer <73441624+melikuomer@users.noreply.github.com>
2025-03-31 00:13:10 +0300

    added precommit compability

---
+++ pre_commit_hooks/post-code-summary.py
@@ -0,0 +1,16 @@
+
+import os
+def main():
+
+    import subprocess
+    diff_input = subprocess.run(['git', 'diff', '-U5'], capture_output=True, text=True).stdout
+
+    from ui import display_artifact
+    from client import App
+    client = App(api_key=os.getenv('GOOGLE_API_KEY') or "")
+    artifact = client.run(diff_input)
+    display_artifact(artifact)
+    return 0
+
+if __name__ == "__main__":
+    main()
